6532e5a1878a54825bf4303c9680cb55afa88416
David Korth <gerbilsoft@gerbilsoft.com>
2018-01-20 03:18:15 -0500

    Enable unit tests in travis-ci and appveyor.

--- travis.sh
+++ travis.sh
@@ -6,7 +6,12 @@ cmake --version
 cmake .. \
 	-DCMAKE_INSTALL_PREFIX=/usr \
 	-DCMAKE_BUILD_TYPE=Release \
+	-DBUILD_TESTING=ON \
 	|| exit 1
 # Build everything.
 make -k || RET=1
+# Test with en_US.UTF8.
+LC_ALL="en_US.UTF8" ctest -V || RET=1
+# Test with fr_FR.UTF8 to find i18n issues.
+LC_ALL="fr_FR.UTF8" ctest -V || RET=1
 exit "${RET}"
