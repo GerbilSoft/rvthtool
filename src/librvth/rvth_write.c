GerbilSoft/rvthtool

commit c880d5268174ef548824483c9a5f8f541a36c7f8

[librvth] rvth_write.c, rvth_copy_to_hdd: Bank 1 is smaller if the bank table is extended.
rvth.c, rvth_init_BankEntry(): Bank 1 size for extended bank tables
is NHCD_EXTBANKTABLE_BANK_1_SIZE_LBA, not NHCD_BANK_GCN_SIZE_NR_LBA.

src/librvth/rvth_write.c
@@ -582,7 +582,17 @@ int rvth_copy_to_hdd(RvtH *rvth_dest, unsigned int bank_dest, const RvtH *rvth_s
 	// TODO: Dual-layer Wii support.
 	if (entry_src->lba_len > NHCD_BANK_SIZE_LBA) {
 		return RVTH_ERROR_IMAGE_TOO_BIG;
-	}
+	} else if (bank_dest == 0) {
+		// Special handling for bank 1 if the bank table is extended.
+		// TODO: entry_dest->lba_len should be the full bank size
+		// if the bank is empty or deleted.
+		// TODO: Add a separate field, lba_max_len?
+		if (rvth_get_BankCount(rvth_dest) > 8) {
+			// Image cannot be larger than NHCD_EXTBANKTABLE_BANK_1_SIZE_LBA.
+			if (entry_src->lba_len > NHCD_EXTBANKTABLE_BANK_1_SIZE_LBA) {
+				return RVTH_ERROR_IMAGE_TOO_BIG;
+			}
+		}
 
 	// Destination bank must be either empty or deleted.
 	entry_dest = &rvth_dest->entries[bank_dest];

src/librvth/rvth.c
@@ -563,7 +563,7 @@ static int rvth_init_BankEntry(RvtH_BankEntry *entry, RefFile *f_img,
 			// This is a relocated Bank 1 on a device with
 			// an extended bank table, so it can only support
 			// GCN disc images.
-			lba_len = NHCD_BANK_GCN_SIZE_NR_LBA;
+			lba_len = NHCD_EXTBANKTABLE_BANK_1_SIZE_LBA;
 		} else {
 			// Use the default LBA length based on bank type.
 			switch (type) {

commit ac04139cf85e12b1420e2db14ad70f40da1460fd

[librvth] rvth_write.c: Added back a missing '}'.
I accidentally deleted this after testing but before committing.

This fixes a regression from commit c880d5268174ef548824483c9a5f8f541a36c7f8.
[librvth] rvth_write.c, rvth_copy_to_hdd: Bank 1 is smaller if the bank table is extended.)

src/librvth/rvth_write.c
@@ -593,6 +593,7 @@ int rvth_copy_to_hdd(RvtH *rvth_dest, unsigned int bank_dest, const RvtH *rvth_s
 				return RVTH_ERROR_IMAGE_TOO_BIG;
 			}
 		}
+	}
 
 	// Destination bank must be either empty or deleted.
 	entry_dest = &rvth_dest->entries[bank_dest];