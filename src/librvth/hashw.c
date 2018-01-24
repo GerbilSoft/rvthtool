f94e12c65db2dbba3e69055fd297f158e90b4111
David Korth <gerbilsoft@gerbilsoft.com>
2018-01-23 23:08:34 -0500

    [librvth] hashw.h: Added a hash object/context mechanism.
    Fakesigning and converting from unencrypted to encrypted requires a lot
    of hashing. Creating a hash object once and reusing it allows us to get
    rid of some of the overhead involved in the SHA-1 code.
    
    hashw_sha1() is now a wrapper function which can still be used for
    simple SHA-1 calculations, e.g. determining if a ticket or TMD is
    fakesigned.

--- src/librvth/hashw.c
+++ src/librvth/hashw.c
@@ -18,55 +18,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
  ***************************************************************************/
 
-#ifndef __RVTHTOOL_LIBRVTH_HASHW_H__
-#define __RVTHTOOL_LIBRVTH_HASHW_H__
+#include "hashw.h"
 
-#include <stdint.h>
-#include <stddef.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-// SHA-1 hash length.
-#define SHA1_HASH_LENGTH 20
-
-struct _HashCtx_SHA1;
-typedef struct _HashCtx_SHA1 HashCtx_SHA1;
-
-/**
- * Create an SHA-1 hash object.
- * @return SHA-1 hash object, or NULL on error.
- */
-HashCtx_SHA1 *hashw_sha1_new(void);
-
-/**
- * Free an SHA-1 hash object.
- * @param sha1 SHA-1 hash object.
- */
-void hashw_sha1_free(HashCtx_SHA1 *sha1);
-
-/**
- * Update an SHA-1 hash object with new data.
- * @param sha1	[in] SHA-1 hash object.
- * @param data	[in] Data to hash.
- * @param size	[in] Size of `data`.
- * @return 0 on success; negative POSIX error code on error.
- */
-int hashw_sha1_update(HashCtx_SHA1 *sha1, const uint8_t *data, size_t size);
-
-/**
- * Calculate the final SHA-1 hash.
- *
- * NOTE: The SHA-1 hash object is NOT freed when calling this function.
- * Its internal state will be reset and it will be usable for calculating
- * another SHA-1 hash.
- *
- * @param sha1		[in] SHA-1 hash object.
- * @param pHash		[out] SHA-1 hash buffer. (Must be SHA1_HASH_LENGTH bytes.)
- * @return 0 on success; negative POSIX error code on error.
- */
-int hashw_sha1_finish(HashCtx_SHA1 *sha1, uint8_t *pHash);
+#include <errno.h>
 
 /**
  * Calculate an SHA-1 hash.
@@ -79,10 +33,19 @@ int hashw_sha1_finish(HashCtx_SHA1 *sha1, uint8_t *pHash);
  * @param size	[in] Size of `data`.
  * @return 0 on success; negative POSIX error code on error.
  */
-int hashw_sha1(uint8_t *pHash, const uint8_t *data, size_t size);
-
-#ifdef __cplusplus
+int hashw_sha1(uint8_t *pHash, const uint8_t *data, size_t size)
+{
+	int ret;
+	HashCtx_SHA1 *sha1 = hashw_sha1_new();
+	if (!sha1) {
+		return -errno;
+	}
+	ret = hashw_sha1_update(sha1, data, size);
+	if (ret != 0) {
+		hashw_sha1_free(sha1);
+		return ret;
+	}
+	ret = hashw_sha1_finish(sha1, pHash);
+	hashw_sha1_free(sha1);
+	return ret;
 }
-#endif
-
-#endif /* __RVTHTOOL_LIBRVTH_HASHW_H__ */
--- src/librvth/hashw_capi.c
+++ src/librvth/hashw_capi.c
@@ -22,61 +22,126 @@
 
 #include <assert.h>
 #include <errno.h>
+#include <stdlib.h>
 
 #include <windows.h>
 #include <wincrypt.h>
 
+// SHA-1 hash object. (Microsoft CryptoAPI version.)
+struct _HashCtx_SHA1 {
+	HCRYPTPROV hProv;
+	HCRYPTHASH hHash;
+};
+
 /**
- * Calculate an SHA-1 hash.
- * @param hash	[out] SHA-1 hash buffer. (Must be SHA1_HASH_LENGTH bytes.)
+ * Create an SHA-1 hash object.
+ * @return SHA-1 hash object, or NULL on error.
+ */
+HashCtx_SHA1 *hashw_sha1_new(void)
+{
+	// Allocate a hash context.
+	HashCtx_SHA1 *sha1 = malloc(sizeof(*sha1));
+	if (!sha1) {
+		// Could not allocate memory.
+		errno = ENOMEM;
+		return NULL;
+	}
+
+	// Open the crypto provider.
+	if (!CryptAcquireContext(&sha1->hProv, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT | CRYPT_SILENT)) {
+		// TODO: Convert GetLastError() to errno?
+		free(sha1);
+		errno = EIO;
+		return NULL;
+	}
+
+	// Create an SHA-1 hash.
+	if (!CryptCreateHash(sha1->hProv, CALG_SHA1, 0, 0, &sha1->hHash)) {
+		// TODO: Convert GetLastError() to errno?
+		CryptReleaseContext(sha1->hProv, 0);
+		free(sha1);
+		errno = EIO;
+		return NULL;
+	}
+
+	// SHA-1 hash context has been initialized.
+	return sha1;
+}
+
+/**
+ * Free an SHA-1 hash object.
+ * @param sha1 SHA-1 hash object.
+ */
+void hashw_sha1_free(HashCtx_SHA1 *sha1)
+{
+	if (sha1) {
+		if (sha1->hHash != 0) {
+			CryptDestroyHash(sha1->hHash);
+		}
+		CryptReleaseContext(sha1->hProv, 0);
+		free(sha1);
+	}
+}
+
+/**
+ * Update an SHA-1 hash object with new data.
+ * @param sha1	[in] SHA-1 hash object.
  * @param data	[in] Data to hash.
  * @param size	[in] Size of `data`.
  * @return 0 on success; negative POSIX error code on error.
  */
-int hashw_sha1(uint8_t *hash, const uint8_t *data, size_t size)
+int hashw_sha1_update(HashCtx_SHA1 *sha1, const uint8_t *data, size_t size)
 {
-	// Reference: https://msdn.microsoft.com/en-us/library/windows/desktop/aa382380%28v=vs.85%29.aspx
-	BOOL bResult = FALSE;
-	HCRYPTPROV hProv = 0;
-	HCRYPTHASH hHash = 0;
-	DWORD cbHash;
-	BOOL bRet;
-
-	assert(hash != NULL);
-	assert(data != NULL);
-	assert(size != 0);
-
-	if (!hash || !data || size == 0) {
-		// Invalid parameters.
+	if (!sha1 || !data) {
 		return -EINVAL;
+	} else if (size == 0) {
+		// Nothing to hash...
+		return 0;
 	}
 
-	// Open the crypto provider.
-	if (!CryptAcquireContext(&hProv, NULL, NULL, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT | CRYPT_SILENT)) {
-		// TODO: Convert GetLastError() to errno.
-		return -EIO;
-	}
-
-	// Create an SHA-1 hash.
-	if (!CryptCreateHash(hProv, CALG_SHA1, 0, 0, &hHash)) {
-		// TODO: Convert GetLastError() to errno.
-		CryptReleaseContext(hProv, 0);
-		return -EIO;
+	if (sha1->hHash == 0) {
+		// Create a new hash.
+		if (!CryptCreateHash(sha1->hProv, CALG_SHA1, 0, 0, &sha1->hHash)) {
+			// TODO: Convert GetLastError() to errno.
+			return -EIO;
+		}
 	}
 
 	// Hash the data.
-	if (!CryptHashData(hHash, data, size, 0)) {
+	if (!CryptHashData(sha1->hHash, data, size, 0)) {
 		// TODO: Convert GetLastError() to errno.
-		CryptDestroyHash(hHash);
-		CryptReleaseContext(hProv, 0);
 		return -EIO;
+	}
+
+	// Data hashed successfully.
+	return 0;
+}
+
+/**
+ * Calculate the final SHA-1 hash.
+ *
+ * NOTE: The SHA-1 hash object is NOT freed when calling this function.
+ * Its internal state will be reset and it will be usable for calculating
+ * another SHA-1 hash.
+ *
+ * @param sha1		[in] SHA-1 hash object.
+ * @param pHash		[out] SHA-1 hash buffer. (Must be SHA1_HASH_LENGTH bytes.)
+ * @return 0 on success; negative POSIX error code on error.
+ */
+int hashw_sha1_finish(HashCtx_SHA1 *sha1, uint8_t *pHash)
+{
+	DWORD cbHash;
+	BOOL bRet;
+
+	if (!sha1 || !pHash) {
+		return -EINVAL;
 	}
 
 	// Get the hash value.
 	cbHash = SHA1_HASH_LENGTH;
-	bRet = CryptGetHashParam(hHash, HP_HASHVAL, hash, &cbHash, 0);
-	CryptDestroyHash(hHash);
-	CryptReleaseContext(hProv, 0);
+	bRet = CryptGetHashParam(sha1->hHash, HP_HASHVAL, pHash, &cbHash, 0);
+	CryptDestroyHash(sha1->hHash);
+	sha1->hHash = 0;
 
 	if (!bRet) {
 		// TODO: Convert GetLastError() to errno.
