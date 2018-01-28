GerbilSoft/rvthtool

commit 11eb07287a526659e0536e10d491fc29c5087aa6

[rvthtool] main.c: If the "command" contains a slash or dot (or backslash on Windows), assume it's a filename and handle it as 'list'.
This should make rvthtool a bit easier to use.

src/rvthtool/main.c
@@ -127,11 +127,35 @@ int RVTH_CDECL _tmain(int argc, TCHAR *argv[])
 		}
 		ret = undelete_bank(argv[2], argv[3]);
 	} else {
-		// TODO: If it's a filename, handle it as "list".
-		fputs("*** ERROR: Unrecognized command: '", stderr);
-		_fputts(argv[1], stderr);
-		fputs("'\n", stderr);
-		return EXIT_FAILURE;
+		// If the "command" contains a slash or dot (or backslash on Windows),
+		// assume it's a filename and handle it as 'list'.
+		const TCHAR *p = argv[1];
+		bool isFilename = false;
+		while (*p != 0) {
+			if (*p == _T('/') || *p == _T('.')) {
+				// Probably a filename.
+				isFilename = true;
+				break;
+			}
+#ifdef _WIN32
+			else if (*p == '\\') {
+				// Probably a filename on Windows.
+				isFilename = true;
+				break;
+			}
+#endif /* _WIN32 */
+		}
+
+		if (isFilename) {
+			// Probably a filename.
+			ret = list_banks(argv[1]);
+		} else {
+			// Not a filename.
+			fputs("*** ERROR: Unrecognized command: '", stderr);
+			_fputts(argv[1], stderr);
+			fputs("'\n", stderr);
+			ret = EXIT_FAILURE;
+		}
 	}
 
 	return ret;

commit 4ee4af4264f376d0a04f3fa4a7539d0aaeb6d728

[rvthtool] main.c: Actually iterate over argv[1] instead of infinitely looping.
This fixes a regression from commit 11eb07287a526659e0536e10d491fc29c5087aa6.
([rvthtool] main.c: If the "command" contains a slash or dot (or backslash on Windows), assume it's a filename and handle it as 'list'.)

src/rvthtool/main.c
@@ -129,9 +129,9 @@ int RVTH_CDECL _tmain(int argc, TCHAR *argv[])
 	} else {
 		// If the "command" contains a slash or dot (or backslash on Windows),
 		// assume it's a filename and handle it as 'list'.
-		const TCHAR *p = argv[1];
+		const TCHAR *p;
 		bool isFilename = false;
-		while (*p != 0) {
+		for (p = argv[1]; *p != 0; p++) {
 			if (*p == _T('/') || *p == _T('.')) {
 				// Probably a filename.
 				isFilename = true;