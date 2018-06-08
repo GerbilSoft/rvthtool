3d2e9c08e6d98df4daec7f31179c246c2c7ab474
David Korth <gerbilsoft@gerbilsoft.com>
2018-06-07 23:10:29 -0400

    [qrvthtool] Copied over QTreeViewOpt from GCN MemCard Recover.

---
+++ src/qrvthtool/widgets/QTreeViewOpt.hpp
@@ -0,0 +1,50 @@
+/***************************************************************************
+ * RVT-H Tool (qrvthtool)                                                  *
+ * QTreeViewOpt.hpp: QTreeView with drawing optimizations.                 *
+ * Specifically, don't update rows that are offscreen.                     *
+ *                                                                         *
+ * Copyright (c) 2013-2018 by David Korth.                                 *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ ***************************************************************************/
+
+#ifndef __RVTHTOOL_QRVTHTOOL_WIDGETS_QTREEVIEWOPT_HPP__
+#define __RVTHTOOL_QRVTHTOOL_WIDGETS_QTREEVIEWOPT_HPP__
+
+// Qt includes and classes.
+#include <QTreeView>
+class QKeyEvent;
+class QFocusEvent;
+
+class QTreeViewOpt : public QTreeView
+{
+	Q_OBJECT
+	typedef QTreeView super;
+
+	public:
+		explicit QTreeViewOpt(QWidget *parent = 0);
+
+	private:
+		Q_DISABLE_COPY(QTreeViewOpt);
+
+	public:
+		void dataChanged(const QModelIndex &topLeft,
+			const QModelIndex &bottomRight,
+			const QVector<int> &roles = QVector<int>()) final;
+
+	protected slots:
+		void showColumnContextMenu(const QPoint &point);
+};
+
+#endif /* __RVTHTOOL_QRVTHTOOL_WIDGETS_QTREEVIEWOPT_HPP__ */
