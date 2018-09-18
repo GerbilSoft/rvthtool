GerbilSoft/rvthtool

commit 13b00700e23eb26dccba8f77048fa4097d22d0c6

[librvth] ref_get_size(): New function to get a file's size.
Windows apparently doesn't like SEEK_END on devices (or at least a
UCRT update broke it). Use DeviceIoControl() instead.

TODO: Add a flag indicating if a file was deleted in bank table only
or if the disc header was wiped. Also make it possible to read from
an RVT-H Reader with an empty bank table. (assume defaults)

src/librvth/reader_plain.c
@@ -97,16 +97,15 @@ Reader *reader_plain_open(RefFile *file, uint32_t lba_start, uint32_t lba_len)
 	reader->vtbl = &reader_plain_vtable;
 
 	// Get the file size.
-	ret = ref_seeko(reader->file, 0, SEEK_END);
-	if (ret != 0) {
-		// Seek error.
+	filesize = ref_get_size(reader->file);
+	if (filesize <= 0) {
+		// Empty file or seek error.
 		err = errno;
 		if (err == 0) {
 			err = EIO;
 		}
 		goto fail;
 	}
-	filesize = ref_tello(reader->file);
 
 	// Set the LBAs.
 	if (lba_start == 0 && lba_len == 0) {

src/librvth/reader_ciso.c
@@ -189,17 +189,15 @@ Reader *reader_ciso_open(RefFile *file, uint32_t lba_start, uint32_t lba_len)
 	// Set the LBAs.
 	if (lba_start == 0 && lba_len == 0) {
 		// Determine the maximum LBA.
-		int64_t offset;
-		ret = ref_seeko(file, 0, SEEK_END);
-		if (ret != 0) {
-			// Seek error.
+		int64_t offset = ref_get_size(file);
+		if (offset <= 0) {
+			// Empty file and/or seek error.
 			err = errno;
 			if (err == 0) {
 				err = EIO;
 			}
 			goto fail;
 		}
-		offset = ref_tello(file);
 
 		// NOTE: If not a multiple of the LBA size,
 		// the partial LBA will be ignored.

src/librvth/reader_wbfs.c
@@ -412,17 +412,15 @@ Reader *reader_wbfs_open(RefFile *file, uint32_t lba_start, uint32_t lba_len)
 	// Set the LBAs.
 	if (lba_start == 0 && lba_len == 0) {
 		// Determine the maximum LBA.
-		int64_t offset;
-		ret = ref_seeko(file, 0, SEEK_END);
-		if (ret != 0) {
-			// Seek error.
+		int64_t offset = ref_get_size(file);
+		if (offset <= 0) {
+			// Empty file and/or seek error.
 			err = errno;
 			if (err == 0) {
 				err = EIO;
 			}
 			goto fail;
 		}
-		offset = ref_tello(file);
 
 		// NOTE: If not a multiple of the LBA size,
 		// the partial LBA will be ignored.

src/librvth/ref_file.c
@@ -299,3 +299,70 @@ int ref_make_sparse(RefFile *f, int64_t size)
 
 	return 0;
 }
+
+/**
+ * Get the size of the file.
+ * @param f RefFile*.
+ * @return Size of file, or -1 on error.
+ */
+int64_t ref_get_size(RefFile *f)
+{
+	// NOTE: _fseeki64(fp, 0, SEEK_END) isn't working on
+	// device files on Windows for some reason...
+#ifdef _WIN32
+	HANDLE hDevice;
+	BOOL bRet;
+	GET_LENGTH_INFORMATION gli;
+	DWORD dwBytesReturned = 0;
+
+	if (!ref_is_device(f))
+#endif /* _WIN32 */
+	{
+		// Not a device file. Use ref_seeko()/ref_tello().
+		int64_t orig_pos, ret;
+		int err;
+
+		orig_pos = ref_tello(f);
+		if (orig_pos < 0) {
+			// Error.
+			return -1;
+		}
+		err = ref_seeko(f, 0, SEEK_END);
+		if (err != 0) {
+			// Error.
+			return -1;
+		}
+		ret = ref_tello(f);
+		err = ref_seeko(f, orig_pos, SEEK_SET);
+		if (err != 0) {
+			// Error.
+			return -1;
+		}
+		return ret;
+	}
+
+#ifdef _WIN32
+	// Win32: Use IOCTL instead of ref_seeko()/ref_tello().
+	hDevice = (HANDLE)_get_osfhandle(_fileno(f->file));
+	if (!hDevice || hDevice == INVALID_HANDLE_VALUE) {
+		// Invalid handle...
+		return 0;
+	}
+
+	// Reference: https://docs.microsoft.com/en-us/windows/desktop/api/winioctl/ni-winioctl-ioctl_disk_get_length_info
+	bRet = DeviceIoControl(hDevice,
+		IOCTL_DISK_GET_LENGTH_INFO,	// dwIoControlCode
+		NULL,				// lpInBuffer
+		0,				// nInBufferSize
+		(LPVOID)&gli,			// output buffer
+		(DWORD)sizeof(gli),		// size of output buffer
+		&dwBytesReturned,		// number of bytes returned
+		NULL);				// OVERLAPPED structure
+	if (!bRet || dwBytesReturned != sizeof(gli)) {
+		// Error.
+		return -1;
+	}
+
+	return gli.Length.QuadPart;
+#endif /* _WIN32 */
+}

src/librvth/ref_file.h
@@ -90,6 +90,13 @@ bool ref_is_device(RefFile *f);
  */
 int ref_make_sparse(RefFile *f, int64_t size);
 
+/**
+ * Get the size of the file.
+ * @param f RefFile*.
+ * @return Size of file, or -1 on error.
+ */
+int64_t ref_get_size(RefFile *f);
+
 /** Convenience wrappers for stdio functions. **/
 
 static inline size_t ref_read(void *ptr, size_t size, size_t nmemb, RefFile *stream)

src/librvth/rvth.c
@@ -489,32 +489,28 @@ RvtH *rvth_open(const TCHAR *filename, int *pErr)
 	}
 
 	// Determine if this is an HDD image or a disc image.
-	ret = ref_seeko(f_img, 0, SEEK_END);
-	if (ret != 0) {
-		// Seek error.
-		if (errno == 0) {
+	errno = 0;
+	len = ref_get_size(f_img);
+	if (len <= 0) {
+		// File is empty and/or an I/O error occurred.
+		if (errno = 0) {
 			errno = EIO;
 		}
-		goto end;
-	}
-	len = ref_tello(f_img);
-	if (len == 0) {
-		// File is empty.
-		errno = EIO;
+		if (pErr) {
+			*pErr = -errno;
+		}
 	} else if (len <= 2*LBA_TO_BYTES(NHCD_BANK_SIZE_LBA)) {
 		// Two banks or less.
 		// This is most likely a standalone disc image.
+		errno = 0;
 		rvth = rvth_open_gcm(f_img, pErr);
 	} else {
 		// More than two banks.
 		// This is most likely an RVT-H HDD image.
+		errno = 0;
 		rvth = rvth_open_hdd(f_img, pErr);
 	}
 
-end:
-	if (pErr) {
-		*pErr = -errno;
-	}
 	// If the RvtH object was opened, it will have
 	// called ref_dup() to increment the reference count.
 	ref_close(f_img);

commit fedd31add2823ecb424f5de61878ce346b9b8b78

[librvth] reader_plain_open(): Don't fail if the file is 0 bytes.
This happens when creating new files for extraction.

This fixes a regression from commit 13b00700e23eb26dccba8f77048fa4097d22d0c6.
([librvth] ref_get_size(): New function to get a file's size.)

src/librvth/reader_plain.c
@@ -96,9 +96,12 @@ Reader *reader_plain_open(RefFile *file, uint32_t lba_start, uint32_t lba_len)
 	reader->vtbl = &reader_plain_vtable;
 
 	// Get the file size.
+	errno = 0;
 	filesize = ref_get_size(reader->file);
-	if (filesize <= 0) {
-		// Empty file or seek error.
+	if (filesize < 0) {
+		// Seek error.
+		// NOTE: Not failing on empty file, since that happens
+		// when creating a new file to extract an image.
 		err = errno;
 		if (err == 0) {
 			err = EIO;