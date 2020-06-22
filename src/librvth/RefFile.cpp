9171d4c3c34ae2fe4cc543e35a7652db59d10946
David Korth <gerbilsoft@gerbilsoft.com>
2020-06-21 22:52:53 -0400

    [librvth] RefFile: Use O_SYNC when writing on Linux.
    Otherwise, it might end up writing gigabytes nearly "instantaneously",
    but the actual writes only occur when the program exits (rvthtool) or
    the RVT-H Reader is closed (qrvthtool).
    
    TODO: Windows equivalent; check other systems.

--- src/librvth/RefFile.cpp
+++ src/librvth/RefFile.cpp
@@ -38,6 +38,7 @@
 # include <sys/ioctl.h>
 # include <sys/types.h>
 # include <sys/stat.h>
+# include <fcntl.h>
 # include <unistd.h>
 # ifdef __linux__
 #  include <linux/fs.h>
@@ -117,12 +118,30 @@ int RefFile::makeWritable(void)
 
 	// Get the current position.
 	int64_t pos = ftello(m_file);
+	bool device = isDevice();
 
 	// Close and reopen the file as writable.
 	// TODO: Potential race condition...
 	int ret = 0;
 	fclose(m_file);
-	m_file = _tfopen(m_filename.c_str(), _T("rb+"));
+	m_file = nullptr;
+	// TODO: Use O_DIRECT equivalent on Windows?
+#ifndef _WIN32
+	if (device) {
+		// Open using O_SYNC.
+		int fd = open(m_filename.c_str(), O_RDWR | O_SYNC);
+		if (fd >= 0) {
+			m_file = fdopen(fd, "rb+");
+			if (!m_file) {
+				close(fd);
+			}
+		}
+	} else
+#endif /* !_WIN32 */
+	{
+		m_file = _tfopen(m_filename.c_str(), _T("rb+"));
+	}
+
 	if (m_file) {
 		// File reopened as writable.
 		m_isWritable = true;
