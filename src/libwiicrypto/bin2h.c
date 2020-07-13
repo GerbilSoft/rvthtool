c61413c0c51d9ab6e18e3a5e2c250b99503ccc86
David Korth <gerbilsoft@gerbilsoft.com>
2020-07-13 02:16:19 -0400

    [libwiicrypto] bin2h.c: Remove #include <malloc.h>.
    It's not available on Mac OS X, and it shouldn't be needed elsewhere.

--- src/libwiicrypto/bin2h.c
+++ src/libwiicrypto/bin2h.c
@@ -18,7 +18,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 #include <errno.h>
-#include <malloc.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
