0800b9315630e744be7f6d3bd7c70f72c8d85548
David Korth <gerbilsoft@gerbilsoft.com>
2022-02-12 00:42:25 -0500

    [cmake] FindNettle.cmake: s/NETTLE_/Nettle_/g
    CMake was complaining about this:
    
    CMake Warning (dev) at /usr/share/cmake/Modules/FindPackageHandleStandardArgs.cmake:438 (message):
      The package name passed to `find_package_handle_standard_args` (NETTLE)
      does not match the name of the calling package (Nettle).  This can lead to
      problems in calling code that expects `find_package` result variables
      (e.g., `_FOUND`) to follow a certain pattern.
    Call Stack (most recent call first):
      cmake/modules/FindNettle.cmake:22 (find_package_handle_standard_args)
      src/CMakeLists.txt:22 (FIND_PACKAGE)
    This warning is for project developers.  Use -Wno-dev to suppress it.

--- cmake/modules/FindNettle.cmake
+++ cmake/modules/FindNettle.cmake
@@ -1,28 +1,28 @@
 # Find the Nettle library.
 # Reference: https://github.com/tatsuhiro-t/wslay/blob/9fe71b1d77823fbca70538bb88a56aec61fecfb3/cmake/FindNettle.cmake
 
-# NETTLE_INCLUDE_DIRS - where to find <nettle/sha.h>, etc.
-# NETTLE_LIBRARIES - List of libraries when using libnettle.
-# NETTLE_FOUND - True if libnettle found.
+# Nettle_INCLUDE_DIRS - where to find <nettle/sha.h>, etc.
+# Nettle_LIBRARIES - List of libraries when using libnettle.
+# Nettle_FOUND - True if libnettle found.
 
-IF(NOT TARGET nettle_dll_target)
+IF(NOT TARGET Nettle_dll_target)
 IF(NOT WIN32)
-	if(NETTLE_INCLUDE_DIRS)
+	if(Nettle_INCLUDE_DIRS)
 		# Already in cache, be silent
-		set(NETTLE_FIND_QUIETLY YES)
+		set(Nettle_FIND_QUIETLY YES)
 	endif()
 
-	find_path(NETTLE_INCLUDE_DIRS nettle/md5.h nettle/ripemd160.h nettle/sha.h)
-	find_library(NETTLE_LIBRARY NAMES nettle libnettle)
-	find_library(HOGWEED_LIBRARY NAMES hogweed libhogweed)
+	find_path(Nettle_INCLUDE_DIRS nettle/md5.h nettle/ripemd160.h nettle/sha.h)
+	find_library(Nettle_LIBRARY NAMES nettle libnettle)
+	find_library(Hogweed_LIBRARY NAMES hogweed libhogweed)
 
-	# handle the QUIETLY and REQUIRED arguments and set NETTLE_FOUND to TRUE if
+	# handle the QUIETLY and REQUIRED arguments and set Nettle_FOUND to TRUE if
 	# all listed variables are TRUE
 	include(FindPackageHandleStandardArgs)
-	find_package_handle_standard_args(NETTLE DEFAULT_MSG NETTLE_LIBRARY NETTLE_INCLUDE_DIRS)
+	find_package_handle_standard_args(Nettle DEFAULT_MSG Nettle_LIBRARY Nettle_INCLUDE_DIRS)
 
-	if(NETTLE_FOUND)
-		set(NETTLE_LIBRARIES ${HOGWEED_LIBRARY} ${NETTLE_LIBRARY})
+	if(Nettle_FOUND)
+		set(Nettle_LIBRARIES ${Hogweed_LIBRARY} ${Nettle_LIBRARY})
 	endif()
 ELSE(NOT WIN32)
 	# Use the included Win32 build of Nettle.
@@ -35,20 +35,20 @@ ELSE(NOT WIN32)
 		MESSAGE(FATAL_ERROR "Unsupported CPU architecture, please fix!")
 	ENDIF()
 
-	SET(NETTLE_WIN32_BASE_PATH "${CMAKE_SOURCE_DIR}/extlib/nettle.win32")
-	SET(NETTLE_INCLUDE_DIRS "${NETTLE_WIN32_BASE_PATH}/include")
+	SET(Nettle_WIN32_BASE_PATH "${CMAKE_SOURCE_DIR}/extlib/nettle.win32")
+	SET(Nettle_INCLUDE_DIRS "${Nettle_WIN32_BASE_PATH}/include")
 	IF(MSVC)
-		SET(NETTLE_LIBRARY "${NETTLE_WIN32_BASE_PATH}/lib.${arch}/libnettle-8.lib")
-		SET(HOGWEED_LIBRARY "${NETTLE_WIN32_BASE_PATH}/lib.${arch}/libhogweed-6.lib")
+		SET(Nettle_LIBRARY "${Nettle_WIN32_BASE_PATH}/lib.${arch}/libnettle-8.lib")
+		SET(Hogweed_LIBRARY "${Nettle_WIN32_BASE_PATH}/lib.${arch}/libhogweed-6.lib")
 	ELSE(MSVC)
-		SET(NETTLE_LIBRARY "${NETTLE_WIN32_BASE_PATH}/lib.${arch}/libnettle.dll.a")
-		SET(HOGWEED_LIBRARY "${NETTLE_WIN32_BASE_PATH}/lib.${arch}/libhogweed.dll.a")
+		SET(Nettle_LIBRARY "${Nettle_WIN32_BASE_PATH}/lib.${arch}/libnettle.dll.a")
+		SET(Hogweed_LIBRARY "${Nettle_WIN32_BASE_PATH}/lib.${arch}/libhogweed.dll.a")
 	ENDIF(MSVC)
-	SET(NETTLE_LIBRARIES ${NETTLE_LIBRARY} ${HOGWEED_LIBRARY})
+	SET(Nettle_LIBRARIES ${Nettle_LIBRARY} ${Hogweed_LIBRARY})
 
 	# Copy and install the DLLs.
-	SET(NETTLE_NETTLE_DLL "${NETTLE_WIN32_BASE_PATH}/lib.${arch}/libnettle-8.dll")
-	SET(NETTLE_HOGWEED_DLL "${NETTLE_WIN32_BASE_PATH}/lib.${arch}/libhogweed-6.dll")
+	SET(Nettle_Nettle_DLL "${Nettle_WIN32_BASE_PATH}/lib.${arch}/libnettle-8.dll")
+	SET(Nettle_Hogweed_DLL "${Nettle_WIN32_BASE_PATH}/lib.${arch}/libhogweed-6.dll")
 
 	# Destination directory.
 	# If CMAKE_CFG_INTDIR is set, a Debug or Release subdirectory is being used.
@@ -58,27 +58,27 @@ ELSE(NOT WIN32)
 		SET(DLL_DESTDIR "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}")
 	ENDIF(CMAKE_CFG_INTDIR)
 
-	ADD_CUSTOM_TARGET(nettle_dll_target ALL
-		DEPENDS hogweed_dll_command nettle_dll_command
+	ADD_CUSTOM_TARGET(Nettle_dll_target ALL
+		DEPENDS Hogweed_dll_command Nettle_dll_command
 		)
-	ADD_CUSTOM_COMMAND(OUTPUT nettle_dll_command
+	ADD_CUSTOM_COMMAND(OUTPUT Nettle_dll_command
 		COMMAND ${CMAKE_COMMAND}
 		ARGS -E copy_if_different
-			"${NETTLE_NETTLE_DLL}" "${DLL_DESTDIR}/libnettle-8.dll"
-		DEPENDS nettle_always_rebuild
+			"${Nettle_Nettle_DLL}" "${DLL_DESTDIR}/libnettle-8.dll"
+		DEPENDS Nettle_always_rebuild
 		)
-	ADD_CUSTOM_COMMAND(OUTPUT hogweed_dll_command
+	ADD_CUSTOM_COMMAND(OUTPUT Hogweed_dll_command
 		COMMAND ${CMAKE_COMMAND}
 		ARGS -E copy_if_different
-			"${NETTLE_HOGWEED_DLL}" "${DLL_DESTDIR}/libhogweed-6.dll"
-		DEPENDS nettle_always_rebuild
+			"${Nettle_Hogweed_DLL}" "${DLL_DESTDIR}/libhogweed-6.dll"
+		DEPENDS Nettle_always_rebuild
 		)
-	ADD_CUSTOM_COMMAND(OUTPUT nettle_always_rebuild
+	ADD_CUSTOM_COMMAND(OUTPUT Nettle_always_rebuild
 		COMMAND ${CMAKE_COMMAND}
 		ARGS -E echo
 		)
 
-	INSTALL(FILES "${NETTLE_NETTLE_DLL}" "${NETTLE_HOGWEED_DLL}"
+	INSTALL(FILES "${Nettle_Nettle_DLL}" "${Nettle_Hogweed_DLL}"
 		DESTINATION "${DIR_INSTALL_DLL}"
 		COMPONENT "dll"
 		)
@@ -86,4 +86,4 @@ ELSE(NOT WIN32)
 	UNSET(DLL_DESTDIR)
 	UNSET(arch)
 ENDIF(NOT WIN32)
-ENDIF(NOT TARGET nettle_dll_target)
+ENDIF(NOT TARGET Nettle_dll_target)
