53bdcd6b4f8a1615528c6d3b71a5b5e53616b997
Cynthia <cynthia@coan.dev>
2023-05-04 12:39:55 -0600

    added show-table command, for inspecting NHCDTable
    since you mentioned wanting to add in a command for repairing a
    NHCD Bank Table, I figured a command to inspect your current NHCD
    Bank Table would also be useful.
    
    even in the absence of a repair command, it still would be useful
    to see the bank table status. I was particularly curious if my RVT-H
    had it's bank fully zero'd or if only a few values were unexpected.
    
    this only adds the command to the CLI, as displaying raw bytes in a
    QT window didn't feel like the best way to print that information.
    If you want it to be there a UI before merging I understand, and can try
    playing around with QT to get something decent.

---
+++ src/rvthtool/show-table.cpp
@@ -0,0 +1,99 @@
+/***************************************************************************
+ * RVT-H Tool                                                              *
+ * show-table.cpp: Print out the NHCD Bank Table for debugging an invalid  *
+ * bank                                                                    *
+ *                                                                         *
+ * Copyright (c) 2023 by David Korth.                                      *
+ * SPDX-License-Identifier: GPL-2.0-or-later                               *
+ ***************************************************************************/
+
+#include "config.librvth.h"
+#include "show-table.hpp"
+
+#include "librvth/rvth.hpp"
+#include "librvth/rvth_error.h"
+#include "librvth/nhcd_structs.h"
+
+#include "libwiicrypto/byteswap.h"
+
+static void print_table(NHCD_BankTable_Header* header) {
+	_ftprintf(stdout, _T("- Table Magic (Expected Value: %d): %d\n"), be32_to_cpu(NHCD_BANKTABLE_MAGIC), header->magic);
+	_ftprintf(stdout, _T("- x004 (expected: 0x00000001): %d\n"), header->x004);
+	_ftprintf(stdout, _T("- bank_count (expected: 0x00000008): %d\n"), header->bank_count);
+	_ftprintf(stdout, _T("- x00C (expected: 0x00000000): %d\n"), header->x00C);
+	_ftprintf(stdout, _T("- x010 (expected: 0x002FF000): %d\n"), header->x010);
+
+	_fputts(_T("- unk table:\n"), stdout);
+	for (uint8_t row = 0; row < 12; ++row) {
+		uint16_t idx = row * 41;
+		_ftprintf(stdout, _T("  %d"), header->unk[idx]);
+		idx += 1;
+		for (; idx < (row + 1) * 41; ++idx) {
+			_ftprintf(stdout, _T(" %d"), header->unk[idx]);
+		}
+		_fputtc(_T('\n'), stdout);
+	}
+	_fputtc(_T('\n'), stdout);
+}
+
+/**
+ * 'show-table' command.
+ * @param rvth_filename RVT-H device or disk image filename.
+ * @return 0 on success; non-zero on error.
+ */
+int show_table(const TCHAR *rvth_filename)
+{
+	// Open the disk image.
+	int ret;
+	RvtH *const rvth = new RvtH(rvth_filename, &ret);
+	if (ret != 0 || !rvth->isOpen()) {
+		_ftprintf(stderr, _T("*** ERROR opening RVT-H device '%s': "), rvth_filename);
+		fputs(rvth_error(ret), stderr);
+		_fputtc(_T('\n'), stderr);
+		delete rvth;
+		return ret;
+	}
+
+	// Validate we're talking to an actual HDD image.
+	if (!rvth->isHDD()) {
+		_fputts(_T("*** ERROR can only print table information on an HDD.\n"), stderr);
+		delete rvth;
+		return 1;
+	}
+
+	// Print out the raw bank status.
+	_ftprintf(stdout, _T("[%s] NHCD Bank Table:\n"), rvth_filename);
+	_fputts(_T("- Status: "), stdout);
+	switch (rvth->nhcd_status()) {
+		case NHCD_STATUS_OK:
+			_fputts(_T("OK"), stdout);
+			break;
+		case NHCD_STATUS_UNKNOWN:
+			_fputts(_T("UNKNOWN"), stdout);
+			break;
+		case NHCD_STATUS_MISSING:
+			_fputts(_T("MISSING"), stdout);
+			break;
+		case NHCD_STATUS_HAS_MBR:
+			_fputts(_T("HAS_MBR"), stdout);
+			break;
+		case NHCD_STATUS_HAS_GPT:
+			_fputts(_T("HAS_GPT"), stdout);
+			break;
+		default:
+			_fputts(_T("???"), stdout);
+			break;
+	}
+	_fputtc(_T('\n'), stdout);
+
+	NHCD_BankTable_Header* header = rvth->nhcd_header();
+	if (header == nullptr) {
+		_fputts(_T("*** ERROR failed to read NHCD header image.\n"), stderr);
+		delete rvth;
+		return 1;
+	}
+	print_table(header);
+
+	delete rvth;
+	return 0;
+}
