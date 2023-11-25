0f4d8348136277a3856bba030b5cb9113a03fb0b
David Korth <gerbilsoft@gerbilsoft.com>
2023-11-25 10:43:55 -0500

    [qrvthtool] qrvthtool.cpp: Also fix Qt::CaseInsensitive's position.
    It's an argument for filename.startsWith(), not QLatin1String().
    
    This seemingly didn't cause a problem with QLatin1String, since the
    constructor takes an optional size parameter. It didn't *work*
    correctly, though.
    
    This *does* cause a problem when switching to QStringLiteral.

--- src/qrvthtool/qrvthtool.cpp
+++ src/qrvthtool/qrvthtool.cpp
@@ -119,7 +119,7 @@ int main(int argc, char *argv[])
 		// TODO: Better device file check.
 		const QString &filename = args.at(1);
 #ifdef _WIN32
-		bool isDevice = filename.startsWith(QLatin1String("\\\\.\\PhysicalDrive", Qt::CaseInsensitive));
+		bool isDevice = filename.startsWith(QLatin1String("\\\\.\\PhysicalDrive"), Qt::CaseInsensitive);
 #else /* !_WIN32 */
 		bool isDevice = filename.startsWith(QLatin1String("/dev/"));
 #endif /* _WIN32 */
