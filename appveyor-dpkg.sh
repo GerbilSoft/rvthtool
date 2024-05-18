GerbilSoft/rvthtool

commit 03ce73c612ff3ffdda80d75b467be146373014da

[cmake] CheckNettle2or3.cmake: Convert the macro to a function.
Set HAVE_NETTLE in PARENT_SCOPE.

cmake/libs/CheckNettle2or3.cmake
@@ -2,18 +2,16 @@
 # If either version is found, HAVE_NETTLE will be set.
 # If Nettle v3 is found, HAVE_NETTLE_3 will be set.
 
-MACRO(CHECK_NETTLE_2_OR_3)
+FUNCTION(CHECK_NETTLE_2_OR_3)
 	FIND_PACKAGE(NETTLE)
-	SET(HAVE_NETTLE ${NETTLE_FOUND})
+	SET(HAVE_NETTLE ${NETTLE_FOUND} PARENT_SCOPE)
 	IF(HAVE_NETTLE)
 		# Check if this is Nettle 3.x.
 		# Nettle 3.1 added version.h, which isn't available
 		# in older verisons, so we can't simply check that.
 		INCLUDE(CheckSymbolExists)
-		SET(OLD_CMAKE_REQUIRED_INCLUDES ${CMAKE_REQUIRED_INCLUDES})
-		SET(OLD_CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES})
-		SET(CMAKE_REQUIRED_INCLUDES ${CMAKE_REQUIRED_INCLUDES} ${NETTLE_INCLUDE_DIRS})
-		SET(CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES} ${NETTLE_LIBRARY})
+		SET(CMAKE_REQUIRED_INCLUDES ${NETTLE_INCLUDE_DIRS})
+		SET(CMAKE_REQUIRED_LIBRARIES ${NETTLE_LIBRARY})
 		CHECK_SYMBOL_EXISTS(aes128_set_decrypt_key "nettle/aes.h" HAVE_NETTLE_3)
 		IF(HAVE_NETTLE_3)
 			# Check for Nettle versioning symbols.
@@ -21,12 +19,8 @@ MACRO(CHECK_NETTLE_2_OR_3)
 			CHECK_SYMBOL_EXISTS(NETTLE_VERSION_MAJOR "nettle/version.h" HAVE_NETTLE_VERSION_H)
 			CHECK_SYMBOL_EXISTS(nettle_version_major "nettle/version.h" HAVE_NETTLE_VERSION_FUNCTIONS)
 		ENDIF(HAVE_NETTLE_3)
-		SET(CMAKE_REQUIRED_INCLUDES ${OLD_CMAKE_REQUIRED_INCLUDES})
-		SET(CMAKE_REQUIRED_LIBRARIES ${OLD_CMAKE_REQUIRED_LIBRARIES})
-		UNSET(OLD_CMAKE_REQUIRED_INCLUDES)
-		UNSET(OLD_CMAKE_REQUIRED_LIBRARIES)
 	ELSE(HAVE_NETTLE)
 		# Disable decryption.
 		SET(ENABLE_DECRYPTION OFF CACHE INTERNAL "Enable decryption for newer ROM and disc images." FORCE)
 	ENDIF(HAVE_NETTLE)
-ENDMACRO(CHECK_NETTLE_2_OR_3)
+ENDFUNCTION(CHECK_NETTLE_2_OR_3)

commit 2029a71346198fc1fc946d5853c11f44e6d5b922

[cmake] CheckNettle2or3.cmake: Also export NETTLE_FOUND in the PARENT_SCOPE.
NETTLE_FOUND may be used to determine if libnettle should be
linked to other libraries and/or executables.

This fixes a regression from commit 03ce73c612ff3ffdda80d75b467be146373014da.
([cmake] CheckNettle2or3.cmake: Convert the macro to a function.)

appveyor-dpkg.sh
@@ -13,4 +13,5 @@ apt-get -y install \
 	\
 	qtbase5-dev \
 	qttools5-dev \
-	qttools5-dev-tools
+	qttools5-dev-tools \
+	libkf5widgetsaddons-dev

cmake/libs/CheckNettle2or3.cmake
@@ -6,6 +6,7 @@ FUNCTION(CHECK_NETTLE_2_OR_3)
 	FIND_PACKAGE(NETTLE)
 	SET(HAVE_NETTLE ${NETTLE_FOUND})
 	SET(HAVE_NETTLE ${NETTLE_FOUND} PARENT_SCOPE)
+	SET(NETTLE_FOUND ${NETTLE_FOUND} PARENT_SCOPE)
 	IF(HAVE_NETTLE)
 		# Check if this is Nettle 3.x.
 		# Nettle 3.1 added version.h, which isn't available

src/qrvthtool/CMakeLists.txt
@@ -92,10 +92,10 @@ ELSE()
         SET(QRVTHTOOL_TRANSLATIONS_DIRECTORY "${CMAKE_INSTALL_PREFIX}/${DIR_INSTALL_TRANSLATIONS}")
 ENDIF()
 
-# KDE5: Check for KWidgetsAddons headers for notification sounds.
+# KF5/KF6: Check for KWidgetsAddons headers for notification sounds.
 IF(UNIX AND NOT APPLE)
 	# Reference: http://www.proli.net/2014/06/21/porting-your-project-to-qt5kf5/
-	# Find KDE5 Extra CMake Modules.
+	# Find KF5/KF6 Extra CMake Modules.
 	FIND_PACKAGE(ECM ${ECM_MINIMUM_VERSION} NO_MODULE)
 	IF(ECM_MODULE_PATH AND ECM_KDE_MODULE_DIR)
 		# Make sure ECM's CMake files don't create an uninstall rule.
@@ -104,7 +104,7 @@ IF(UNIX AND NOT APPLE)
 		# Don't add KDE tests to the CTest build.
 		SET(KDE_SKIP_TEST_SETTINGS TRUE)
 
-		# Include KDE5 CMake modules.
+		# Include KF5/KF6 CMake modules.
 		LIST(APPEND CMAKE_MODULE_PATH ${ECM_MODULE_PATH} ${ECM_KDE_MODULE_DIR})
 		# NOTE: The following are not needed for rvthtool and cause
 		# duplicate moc generation, but we're leaving it here for reference.
@@ -116,7 +116,7 @@ IF(UNIX AND NOT APPLE)
 		# NOTE: Not needed because KDECompilerSettings isn't used.
 		#STRING(REPLACE "-std=iso9899:1990" "" CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
 
-		# Find KDE5. (TODO: Version?)
+		# Find KF5/KF6. (TODO: Version?)
 		FIND_PACKAGE(${KF_NS} ${KF_MIN} COMPONENTS WidgetsAddons)
 		IF(${KF_NS}WidgetsAddons_FOUND)
 			SET(HAVE_KF_WidgetsAddons 1)